fb8f00a6efd70334db9d194b38016c41cae8a081
Marbin Tan <tmarbin@vmware.com>
2021-12-23 10:35:38 -0600

    GPV-738 Clean up the options in tpcds_variables.sh
    The SINGLE_USER_ITERATIONS is not part of the benchmark.
    We group it with other 2 options for schema and query and call them misc
    options, since they are addressing misc perspective of the benchmark testing,
    on query iteration time, on query execution statistics, and on table
    distribution.
    
    Co-authored-by: Marbin Tan <tmarbin@vmware.com>
    Co-authored-by: Xin Zhang <zhxin@vmware.com>

--- tpcds_variables.sh
+++ tpcds_variables.sh
@@ -3,13 +3,8 @@ ADMIN_USER="gpadmin"
 INSTALL_DIR="/dsbenchmark"
 
 # benchmark options
-RANDOM_DISTRIBUTION="false"
-MULTI_USER_COUNT="2"
 GEN_DATA_SCALE="1"
-SINGLE_USER_ITERATIONS="1"
-
-# query options
-EXPLAIN_ANALYZE="false"
+MULTI_USER_COUNT="2"
 
 # step options
 RUN_COMPILE_TPCDS="true"
@@ -22,3 +17,8 @@ RUN_SINGLE_USER_REPORT="true"
 RUN_MULTI_USER="true"
 RUN_MULTI_USER_REPORT="true"
 RUN_SCORE="true"
+
+# misc options
+SINGLE_USER_ITERATIONS="1"
+EXPLAIN_ANALYZE="false"
+RANDOM_DISTRIBUTION="false"
