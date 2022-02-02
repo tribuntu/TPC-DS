f2ee1f814cc63ae78f99578343184d75c93536c8
Marbin Tan <tmarbin@vmware.com>
2022-02-02 14:40:33 -0800

    GPV-778 Add QphDS 2.2.0 calculation
    - Use `psql` to do some calculations as doubles
    - Use `bc` for storing intermediate values for the v2.2.0 calculations
      - This is also useful for output at the end of the run
    - Repair an error in displaying v1.3.1's TTT value (it was previously
      displaying only half the value)
    - Aligned values using tabs
    
    Co-authored-by: Marbin Tan <tmarbin@vmware.com>
    Co-authored-by: Stu Pollock <stuartp@vmware.com>

--- 09_score/rollout.sh
+++ 09_score/rollout.sh
@@ -5,11 +5,11 @@ PWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $PWD/../functions.sh
 source_bashrc
 
-GEN_DATA_SCALE=$1
-EXPLAIN_ANALYZE=$2
-RANDOM_DISTRIBUTION=$3
-MULTI_USER_COUNT=$4
-SINGLE_USER_ITERATIONS=$5
+GEN_DATA_SCALE=$1; shift
+EXPLAIN_ANALYZE=$1; shift
+RANDOM_DISTRIBUTION=$1; shift
+MULTI_USER_COUNT=$1; shift
+SINGLE_USER_ITERATIONS=$1; shift
 
 if [[ "$GEN_DATA_SCALE" == "" || "$EXPLAIN_ANALYZE" == "" || "$RANDOM_DISTRIBUTION" == "" || "$MULTI_USER_COUNT" == "" || "$SINGLE_USER_ITERATIONS" == "" ]]; then
 	echo "You must provide the scale as a parameter in terms of Gigabytes, true/false to run queries with EXPLAIN ANALYZE option, true/false to use random distrbution, multi-user count, and the number of sql iterations."
@@ -22,27 +22,48 @@ init_log $step
 
 load_time=$(psql -v ON_ERROR_STOP=1 -q -t -A -c "select round(sum(extract('epoch' from duration))) from tpcds_reports.load where tuples > 0")
 analyze_time=$(psql -v ON_ERROR_STOP=1 -q -t -A -c "select round(sum(extract('epoch' from duration))) from tpcds_reports.load where tuples = 0")
-queries_time=$(psql -v ON_ERROR_STOP=1 -q -t -A -c "select round(sum(extract('epoch' from duration))) from (SELECT split_part(description, '.', 2) AS id,  min(duration) AS duration FROM tpcds_reports.sql GROUP BY split_part(description, '.', 2)) as sub")
+queries_time=$(psql -v ON_ERROR_STOP=1 -q -t -A -c "select round(sum(extract('epoch' from duration))) from (SELECT split_part(description, '.', 2) AS id, min(duration) AS duration FROM tpcds_reports.sql GROUP BY split_part(description, '.', 2)) as sub")
 concurrent_queries_time=$(psql -v ON_ERROR_STOP=1 -q -t -A -c "select round(sum(extract('epoch' from duration))) from tpcds_testing.sql")
 
-q=$(( 3 * MULTI_USER_COUNT * 99 ))
+# Calculate operands for v1.3.1 of the TPC-DS score
+q_1_3_1=$(( 3 * MULTI_USER_COUNT * 99 ))
+tpt_1_3_1=$(( queries_time * MULTI_USER_COUNT ))
+ttt_1_3_1=$(( 2 * concurrent_queries_time ))
+tld_1_3_1=$(( MULTI_USER_COUNT * load_time / 100 ))
 
-tpt=$(( queries_time * MULTI_USER_COUNT ))
-tld=$(( MULTI_USER_COUNT * load_time / 100 ))
+# Calculate operands for v2.2.0 of the TPC-DS score
+q_2_2_0=$(( MULTI_USER_COUNT * 99 ))
+tpt_2_2_0=$(echo "$queries_time * $MULTI_USER_COUNT / 3600" | bc -l)
+ttt_2_2_0=$(echo "2 * $concurrent_queries_time / 3600" | bc -l)
+tld_2_2_0=$(echo "0.01 * $MULTI_USER_COUNT * $load_time / 3600" | bc -l)
 
-num_score=$(( GEN_DATA_SCALE * q ))
-dem_score=$(( tpt + 2 * concurrent_queries_time + tld ))
-score=$(( num_score / dem_score ))
+# Calculate scores using aggregation functions in psql
+psql -v ON_ERROR_STOP=1 -q -t -A -c "drop table if exists tpc_ds_vals"
+psql -v ON_ERROR_STOP=1 -q -t -A -c "create table tpc_ds_vals(v1_3_1 double precision, v2_2_0 double precision)"
+psql -v ON_ERROR_STOP=1 -q -t -A -c "insert into tpc_ds_vals values($tpt_1_3_1,$tpt_2_2_0),($ttt_1_3_1,$ttt_2_2_0),($tld_1_3_1,$tld_2_2_0)"
+score_1_3_1=$(psql -v ON_ERROR_STOP=1 -q -t -A -c "select floor($q_1_3_1 * $GEN_DATA_SCALE / sum(v1_3_1)) from tpc_ds_vals")
+score_2_2_0=$(psql -v ON_ERROR_STOP=1 -q -t -A -c "select floor($q_2_2_0 * $GEN_DATA_SCALE / exp(avg(ln(v2_2_0)))) from tpc_ds_vals")
+psql -v ON_ERROR_STOP=1 -q -t -A -c "drop table tpc_ds_vals"
 
-echo -e "Scale Factor\t$GEN_DATA_SCALE"
-echo -e "Load\t$load_time"
-echo -e "Analyze\t$analyze_time"
-echo -e "1 User Queries\t$queries_time"
+echo -e "Number of Streams (Sq)\t$MULTI_USER_COUNT"
+echo -e "Scale Factor (SF)\t$GEN_DATA_SCALE"
+echo -e "Load\t\t\t$load_time"
+echo -e "Analyze\t\t\t$analyze_time"
+echo -e "1 User Queries\t\t$queries_time"
 echo -e "Concurrent Queries\t$concurrent_queries_time"
-echo -e "Q\t$q"
-echo -e "TPT\t$tpt"
-echo -e "TTT\t$concurrent_queries_time"
-echo -e "TLD\t$tld"
-echo -e "Score\t$score"
+echo -e ""
+echo -e "TPC-DS v1.3.1 (QphDS@SF = floor(SF * Q / sum(TPT, TTT, TLD)))"
+echo -e "Q (3 * Sq * 99)\t\t$q_1_3_1"
+echo -e "TPT (seconds)\t\t$tpt_1_3_1"
+echo -e "TTT (seconds)\t\t$ttt_1_3_1"
+echo -e "TLD (seconds)\t\t$tld_1_3_1"
+echo -e "Score\t\t\t$score_1_3_1"
+echo -e ""
+echo -e "TPC-DS v2.2.0 (QphDS@SF = floor(SF * Q / geomean(TPT, TTT, TLD)))"
+echo -e "Q (Sq * 99)\t\t$q_2_2_0"
+printf "TPT (hours)\t\t%.3f\n" "$tpt_2_2_0"
+printf "TTT (hours)\t\t%.3f\n" "$ttt_2_2_0"
+printf "TLD (hours)\t\t%.3f\n" "$tld_2_2_0"
+echo -e "Score\t\t\t$score_2_2_0"
 
 end_step $step
