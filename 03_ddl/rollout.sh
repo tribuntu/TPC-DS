85058d89a567960bb20d712366b2373f3d0dc517
Jianwen Dong <djianwen@vmware.com>
2023-01-11 11:29:32 -0600

    GPV-1309 Remove RUN_LOAD from rollout.sh of 03_ddl
    When we have RUN_DDL, we should always recreate the tables regardless of
    whether we want to reload the data or not.
    
    Co-authored-by: Jianwen Dong <djianwen@vmware.com>
    Co-authored-by: Xin Zhang <zhxin@vmware.com>

--- 03_ddl/rollout.sh
+++ 03_ddl/rollout.sh
@@ -9,74 +9,72 @@ get_version
 
 filter="gpdb"
 
-if [ "${RUN_LOAD}" == "true" ]; then
-  #Create tables
-  for i in ${PWD}/*.${filter}.*.sql; do
-    start_log
-    id=$(echo ${i} | awk -F '.' '{print $1}')
-    export id
-    schema_name=$(echo ${i} | awk -F '.' '{print $2}')
-    export schema_name
-    table_name=$(echo ${i} | awk -F '.' '{print $3}')
-    export table_name
+#Create tables
+for i in ${PWD}/*.${filter}.*.sql; do
+  start_log
+  id=$(echo ${i} | awk -F '.' '{print $1}')
+  export id
+  schema_name=$(echo ${i} | awk -F '.' '{print $2}')
+  export schema_name
+  table_name=$(echo ${i} | awk -F '.' '{print $3}')
+  export table_name
 
-    if [ "${RANDOM_DISTRIBUTION}" == "true" ]; then
-      DISTRIBUTED_BY="DISTRIBUTED RANDOMLY"
-    else
-      for z in $(cat ${PWD}/distribution.txt); do
-        table_name2=$(echo ${z} | awk -F '|' '{print $2}')
-        if [ "${table_name2}" == "${table_name}" ]; then
-          distribution=$(echo ${z} | awk -F '|' '{print $3}')
-        fi
-      done
-      DISTRIBUTED_BY="DISTRIBUTED BY (${distribution})"
-    fi
-
-    log_time "psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -a -P pager=off -f ${i} -v SMALL_STORAGE=\"${SMALL_STORAGE}\" -v MEDIUM_STORAGE=\"${MEDIUM_STORAGE}\" -v LARGE_STORAGE=\"${LARGE_STORAGE}\" -v DISTRIBUTED_BY=\"${DISTRIBUTED_BY}\""
-    psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -a -P pager=off -f ${i} -v SMALL_STORAGE="${SMALL_STORAGE}" -v MEDIUM_STORAGE="${MEDIUM_STORAGE}" -v LARGE_STORAGE="${LARGE_STORAGE}" -v DISTRIBUTED_BY="${DISTRIBUTED_BY}"
-
-    print_log
-  done
-
-  #external tables are the same for all gpdb
-  get_gpfdist_port
-
-  for i in ${PWD}/*.ext_tpcds.*.sql; do
-    start_log
-
-    id=$(echo ${i} | awk -F '.' '{print $1}')
-    schema_name=$(echo ${i} | awk -F '.' '{print $2}')
-    table_name=$(echo ${i} | awk -F '.' '{print $3}')
-
-    counter=0
-
-    if [ "${VERSION}" == "gpdb_6" ] || [ "${VERSION}" == "gpdb_7" ]; then
-      SQL_QUERY="select rank() over(partition by g.hostname order by g.datadir), g.hostname from gp_segment_configuration g where g.content >= 0 and g.role = '${GPFDIST_LOCATION}' order by g.hostname"
-    else
-      SQL_QUERY="select rank() over (partition by g.hostname order by p.fselocation), g.hostname from gp_segment_configuration g join pg_filespace_entry p on g.dbid = p.fsedbid join pg_tablespace t on t.spcfsoid = p.fsefsoid where g.content >= 0 and g.role = '${GPFDIST_LOCATION}' and t.spcname = 'pg_default' order by g.hostname"
-    fi
-    for x in $(psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -A -t -c "${SQL_QUERY}"); do
-      CHILD=$(echo ${x} | awk -F '|' '{print $1}')
-      EXT_HOST=$(echo ${x} | awk -F '|' '{print $2}')
-      PORT=$((GPFDIST_PORT + CHILD))
-
-      if [ "${counter}" -eq "0" ]; then
-        LOCATION="'"
-      else
-        LOCATION+="', '"
+  if [ "${RANDOM_DISTRIBUTION}" == "true" ]; then
+    DISTRIBUTED_BY="DISTRIBUTED RANDOMLY"
+  else
+    for z in $(cat ${PWD}/distribution.txt); do
+      table_name2=$(echo ${z} | awk -F '|' '{print $2}')
+      if [ "${table_name2}" == "${table_name}" ]; then
+        distribution=$(echo ${z} | awk -F '|' '{print $3}')
       fi
-      LOCATION+="gpfdist://${EXT_HOST}:${PORT}/${table_name}_[0-9]*_[0-9]*.dat"
-
-      counter=$((counter + 1))
     done
-    LOCATION+="'"
+    DISTRIBUTED_BY="DISTRIBUTED BY (${distribution})"
+  fi
 
-    log_time "psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -a -P pager=off -f ${i} -v LOCATION=\"${LOCATION}\""
-    psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -a -P pager=off -f ${i} -v LOCATION="${LOCATION}"
+  log_time "psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -a -P pager=off -f ${i} -v SMALL_STORAGE=\"${SMALL_STORAGE}\" -v MEDIUM_STORAGE=\"${MEDIUM_STORAGE}\" -v LARGE_STORAGE=\"${LARGE_STORAGE}\" -v DISTRIBUTED_BY=\"${DISTRIBUTED_BY}\""
+  psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -a -P pager=off -f ${i} -v SMALL_STORAGE="${SMALL_STORAGE}" -v MEDIUM_STORAGE="${MEDIUM_STORAGE}" -v LARGE_STORAGE="${LARGE_STORAGE}" -v DISTRIBUTED_BY="${DISTRIBUTED_BY}"
 
-    print_log
+  print_log
+done
+
+#external tables are the same for all gpdb
+get_gpfdist_port
+
+for i in ${PWD}/*.ext_tpcds.*.sql; do
+  start_log
+
+  id=$(echo ${i} | awk -F '.' '{print $1}')
+  schema_name=$(echo ${i} | awk -F '.' '{print $2}')
+  table_name=$(echo ${i} | awk -F '.' '{print $3}')
+
+  counter=0
+
+  if [ "${VERSION}" == "gpdb_6" ] || [ "${VERSION}" == "gpdb_7" ]; then
+    SQL_QUERY="select rank() over(partition by g.hostname order by g.datadir), g.hostname from gp_segment_configuration g where g.content >= 0 and g.role = '${GPFDIST_LOCATION}' order by g.hostname"
+  else
+    SQL_QUERY="select rank() over (partition by g.hostname order by p.fselocation), g.hostname from gp_segment_configuration g join pg_filespace_entry p on g.dbid = p.fsedbid join pg_tablespace t on t.spcfsoid = p.fsefsoid where g.content >= 0 and g.role = '${GPFDIST_LOCATION}' and t.spcname = 'pg_default' order by g.hostname"
+  fi
+  for x in $(psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -A -t -c "${SQL_QUERY}"); do
+    CHILD=$(echo ${x} | awk -F '|' '{print $1}')
+    EXT_HOST=$(echo ${x} | awk -F '|' '{print $2}')
+    PORT=$((GPFDIST_PORT + CHILD))
+
+    if [ "${counter}" -eq "0" ]; then
+      LOCATION="'"
+    else
+      LOCATION+="', '"
+    fi
+    LOCATION+="gpfdist://${EXT_HOST}:${PORT}/${table_name}_[0-9]*_[0-9]*.dat"
+
+    counter=$((counter + 1))
   done
-fi
+  LOCATION+="'"
+
+  log_time "psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -a -P pager=off -f ${i} -v LOCATION=\"${LOCATION}\""
+  psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -a -P pager=off -f ${i} -v LOCATION="${LOCATION}"
+
+  print_log
+done
 
 DropRole="DROP ROLE IF EXISTS ${BENCH_ROLE}"
 CreateRole="CREATE ROLE ${BENCH_ROLE}"
